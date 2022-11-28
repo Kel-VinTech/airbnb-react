b58886631e91f68eb8857c2545a4510753d93139
Kel-VinTech <mondaykelvintech@gmail.com>
2022-11-27 19:46:35 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import HeroContent from './components/HeroContent';
+import Card from "./components/Card";
 
+{/* <HeroContent /> */}
 function App() {
   return (
     <div className="container">
       <Header />
-      <HeroContent />
+      <Card />
     </div>
   );
 }
