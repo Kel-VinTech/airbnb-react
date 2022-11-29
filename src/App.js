fb0207ac6250d2a08710942e25f211e5a392a339
Kel-VinTech <mondaykelvintech@gmail.com>
2022-11-29 18:40:11 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -2,44 +2,36 @@ import React from "react";
 import Header from "./components/Header";
 import HeroContent from './components/HeroContent';
 import Card from "./components/Card";
+import CardData from "./components/CardData";
 
-// images 
-import image1 from "./img/image 12.png";
-import image2 from "./img/wedding-photography 1.png"
-import image3 from "./img/mountain-bike 1.png"
+
+{/* <HeroContent />  */}
+
+
+
 
 
 function App() {
+  const cards = CardData.map(item => {
+    return (
+        <Card 
+                img={item.img}
+                rating={item.rating}
+                reviewCount = {item.reviewCount}
+                country = {item.country}
+                title = {item.title}
+                price = {item.price}      
+        />
+    )
+  })
+
+
+
   return (
+         
     <div className="container">
       <Header />
-      <HeroContent /> 
-      <Card 
-        img={image1}
-        rating="5.0"
-        reviewCount = {6}
-        country = "USA"
-        title = "Life lesson from Katie"
-        price = {136}
-      />
-      <Card 
-        img={image2}
-        rating="5.0"
-        reviewCount = {30}
-        country = "USA"
-        title = "Learn wedding photography"
-        price = {125}
-      />
-
-<Card 
-        img={image3}
-        rating="4.8"
-        reviewCount = {2}
-        country = "USA"
-        title = "Group Mountain Biking"
-        price = {50}
-      />
-
+      {cards}
     </div>
   );
 }
